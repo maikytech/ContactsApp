9a0b9a0afbbb59c939efc18942e4dbccca14378c
maikytech <maikytech1307@gmail.com>
2022-01-17 21:03:57 -0500

    Configuration is done to support iOS10

---
+++ ContactsApp/Contacts/View/ContactsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ContactsViewController.swift
+//  ContactsApp
+//
+//  Created by Maiqui Cedeño on 17/01/22.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
